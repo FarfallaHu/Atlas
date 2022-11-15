0258335df9464314a027a8f2c38002e3b4601557
Farfalla Hu <farfallahu@gmail.com>
2022-11-15 14:59:47 -0500

    Added alert when no data

--- main.py
+++ main.py
@@ -2,13 +2,22 @@ from util import generate_img
 
 import json
 from flask import request
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
 
 
 app = Flask(
     __name__,
     # static_folder='templates/assets/images',
     )
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+
+@app.after_request
+def add_header(response):
+    # response.cache_control.no_store = True
+    response.headers['Cache-Control'] = 'no-store, no-cache, must-revalidate, post-check=0, pre-check=0, max-age=0'
+    response.headers['Pragma'] = 'no-cache'
+    response.headers['Expires'] = '-1'
+    return response
 
 @app.route('/')
 def index():
@@ -21,22 +30,30 @@ def phpexample():
 
 @app.route('/test', methods=['POST'])
 def test():
+
     output = request.get_json()
-    print(output) # This is the output that was stored in the JSON within the browser
-    print(type(output))
+    print("input:", output) # This is the output that was stored in the JSON within the browser
+    # print(type(output))
     result = json.loads(output) #this converts the json output to a python dictionary
-    print(result) # Printing the new dictionary
-    print(type(result))#this shows the json converted as a python dictionary
-    with open('data.json', 'w') as f:
-        json.dump(result, f)
-    generate_img(
+    print("result:", result) # Printing the new dictionary
+    # print(type(result))#this shows the json converted as a python dictionary
+    #with open('data.json', 'w') as f:
+    #    json.dump(result, f)
+    output = generate_img(
         result['harpnum'],
         result['date'][0:4]+result['date'][5:7]+result['date'][8:10],
         result['hour'],
         result['minute'],
         result['color_option'],
     )
-    return result
+    print("F:", output)
+
+    if output==1:
+        return result
+    else:
+        return "0"
+    #return redirect(url_for('static', filename='images/banner/temp.png'))
+    #return render_template('index.html', static='images/banner/temp.png')
 
 
 
--- util.py
+++ util.py
@@ -14,42 +14,49 @@ def generate_img(harp_num,date,hour,minute,color_option='Grey'):
 
     filename = 'hmi.sharp_cea_720s.'+str(harp_num)+'.'+str(date)+'_'+str(hour)+str(minute)+'00_TAI.magnetogram.fits'
     cursor = collection.find({'FILENAME': filename})
-    for obj in cursor:
-        hdul = fits.HDUList()
+    if len(list(cursor))==1:
+        for obj in list(collection.find({'FILENAME': filename})):
+            hdul = fits.HDUList()
+    
+            primary_header = {}
+            img_header = {}
+            count=0
+            for key, value in obj.items():
+                if key.startswith('P_'):
+                    primary_header[key.split('P_')[1]] = value
+                else:
+                    if key!='_id' and key!='DATA' and key!='FILENAME':
+                        img_header[key] = value
+                        count+=1
+    
+                if key=='DATA':
+                    img_data = pkl.loads(value)
+    
+            compIMG_hdu = fits.CompImageHDU(data=img_data,header=fits.Header(img_header))
 
-        primary_header = {}
-        img_header = {}
-        count=0
-        for key, value in obj.items():
-            if key.startswith('P_'):
-                primary_header[key.split('P_')[1]] = value
-            else:
-                if key!='_id' and key!='DATA' and key!='FILENAME':
-                    img_header[key] = value
-                    count+=1
+            hdul.append(compIMG_hdu)
+            hdul.verify('fix')
+            hdul.writeto('./temp.fits', overwrite=True)
 
-            if key=='DATA':
-                img_data = pkl.loads(value)
-
-        compIMG_hdu = fits.CompImageHDU(data=img_data,header=fits.Header(img_header))
-
-        hdul.append(compIMG_hdu)
-        hdul.writeto('./temp.fits', overwrite=True)
-        
-        fig = plt.figure(figsize=(8,6))
-        hmi_magmap = sunpy.map.Map('./temp.fits')
-        
-        if color_option=='Grey':
-            hmi_magmap.plot()
-        elif color_option=='Color':
-            hmi_magmap.plot_settings['cmap'] = 'hmimag'
-            hmi_magmap.plot_settings['norm'] = plt.Normalize(-1500, 1500)
-            im_hmi = hmi_magmap.plot()
-            cb = plt.colorbar(im_hmi, fraction=0.019, pad=0.1)
-            cb.set_label("LOS Magnetic Field [Gauss]")
-
-        plt.xlabel('Carrington Longitude [deg]',fontsize = 16)
-        plt.ylabel('Latitude [deg]',fontsize = 16)
-        plt.savefig('./static/images/banner/01.png', dpi=150, bbox_inches='tight')
-        #plt.show()
-        plt.close(fig)
+            fig = plt.figure(figsize=(8,6))
+            hmi_magmap = sunpy.map.Map('./temp.fits')
+            
+            if color_option=='Grey':
+                hmi_magmap.plot()
+            elif color_option=='Color':
+                hmi_magmap.plot_settings['cmap'] = 'hmimag'
+                hmi_magmap.plot_settings['norm'] = plt.Normalize(-1500, 1500)
+                im_hmi = hmi_magmap.plot()
+                cb = plt.colorbar(im_hmi, fraction=0.019, pad=0.1)
+                cb.set_label("LOS Magnetic Field [Gauss]")
+    
+            plt.xlabel('Carrington Longitude [deg]',fontsize = 16)
+            plt.ylabel('Latitude [deg]',fontsize = 16)
+            plt.savefig('./static/images/banner/01.png', bbox_inches='tight')
+            plt.switch_backend('Agg')
+            #plt.show()
+            #plt.close(fig)
+            
+            return 1
+    else:
+        return 0
